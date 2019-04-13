ed2e58222514808cc5f5e017f4bdccd2034e6a11
Christopher Murphy <Murphy.Christopher.Paul.gmail.com>
2019-04-13 08:50:50 -0400

    Initial Push of work from 4/12/19 includes pulling in the dataset as an imageset, extracting features and attempting to use the image processing tooolbox on the set.

---
+++ Final Project/finalProject.m
@@ -0,0 +1,14 @@
+clear all
+close all
+clc
+
+setDir = 'C:\Users\Tophe\OneDrive\Documents\MATLAB\CSE881\Final Project\Sorted_Cars_By_Type_15cm_24px-exc_v5-marg-32_expanded\Potsdam\';
+imgSets = imageDatastore(setDir,'IncludeSubfolders',true,'LabelSource','foldernames');
+
+[trainingSet,testSet] = splitEachLabel(imgSets,0.3,'randomize');
+
+bag = bagOfFeatures(trainingSet);
+
+categoryClassifier = trainImageCategoryClassifier(trainingSet,bag);
+confMatrix = evaluate(categoryClassifier,testSet)
+mean(diag(confMatrix))
