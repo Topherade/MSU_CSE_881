55838bc8c2b722744c905c3cecc6f736ab7f4dd0
smurphy0709 <smurphy0709@gmail.com>
2019-04-17 15:01:56 -0400

    Editted picture labels and removed unneeded images

--- Final Project/finalProject.m
+++ Final Project/finalProject.m
@@ -6,27 +6,33 @@ net = resnet50();
 setDir = 'Sorted_Cars_By_Type_15cm_24px-exc_v5-marg-32_expanded/Potsdam/';
 imgSets = imageDatastore(setDir,'IncludeSubfolders',true,'LabelSource',...
     'foldernames');
-[trainingSet, testSet] = splitEachLabel(imgSets, 0.3, 'randomize');
+[train, test] = splitEachLabel(imgSets, 0.5, 'randomize');
 
 imageSize = net.Layers(1).InputSize;
-augmentedTrainingSet = augmentedImageDatastore(imageSize, trainingSet, ...
+aug_train = augmentedImageDatastore(imageSize, train, ...
     'ColorPreprocessing', 'gray2rgb');
-augmentedTestSet = augmentedImageDatastore(imageSize, testSet, ... 
+aug_test = augmentedImageDatastore(imageSize, test, ... 
     'ColorPreprocessing', 'gray2rgb');
 
-featureLayer = 'fc1000';
-trainingFeatures = activations(net, augmentedTrainingSet, featureLayer, ...
+trainFeatures = activations(net, aug_train, 'fc1000', ...
     'MiniBatchSize', 32, 'OutputAs', 'columns');
 
 % Get training labels from the trainingSet
-trainingLabels = trainingSet.Labels;
+trainingLabels = train.Labels;
 
-% Train multiclass SVM classifier using a fast linear solver, and set
-% 'ObservationsIn' to 'columns' to match the arrangement used for training
-% features.
-classifier = fitcecoc(trainingFeatures, trainingLabels, 'Learners', ...
+% Train multiclass SVM classifier using a fast linear solver
+classifier = fitcecoc(trainFeatures, trainingLabels, 'Learners', ...
     'Linear', 'Coding', 'onevsall', 'ObservationsIn', 'columns');
 
+% Extract test features using the CNN
+testFeatures = activations(net, aug_test, 'fc1000', ...
+    'MiniBatchSize', 32, 'OutputAs', 'columns');
 
+% Pass CNN image features to trained classifier
+predictLabels = predict(classifier, testFeatures, 'ObservationsIn', 'columns');
 
-%https://www.mathworks.com/help/vision/examples/image-category-classification-using-deep-learning.html
+% Get the known labels
+testLabels = test.Labels;
+
+% Tabulate the results using a confusion matrix.
+confMat = confusionmat(testLabels, predictLabels)
